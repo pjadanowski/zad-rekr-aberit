426fbd495c2d81ed8d086db59779eb0c7c4dc6b4
Paweł Jadanowski <pjadanowski@gmail.com>
2025-07-14 20:12:47 +0200

    add api resources for Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Product\StoreProductRequest;
+use App\Http\Requests\Product\UpdateProductRequest;
+use App\Http\Resources\Product\ProductResource;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::with('category')->get();
+
+        return ProductResource::collection($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+
+        return new ProductResource($product->load('category'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        $product->loadMissing('category');
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        return new ProductResource($product->load('category'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', App\Http\Controllers\CategoryController::class);
+Route::apiResource('products', App\Http\Controllers\ProductController::class);
