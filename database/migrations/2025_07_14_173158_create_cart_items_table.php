b2c7c08a69791f3ef6be7d93c9a09d8e6c101a10
Paweł Jadanowski <pjadanowski@gmail.com>
2025-07-14 19:42:45 +0200

    add CartItem model and migration

---
+++ database/migrations/2025_07_14_173158_create_cart_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Cart::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
+            $table->unsignedInteger('quantity');
+            $table->unsignedInteger('unit_price');
+            $table->unsignedInteger('total_price');
+            $table->timestamps();
+            $table->unique(['cart_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
