2009665f49a71ae0a2be4691003836ac192eac64
Paweł Jadanowski <pjadanowski@gmail.com>
2025-07-15 09:46:12 +0200

    add Pagination for Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::with('category')->get();
+        $products = Product::with('category')->paginate();
 
         return ProductResource::collection($products);
     }
